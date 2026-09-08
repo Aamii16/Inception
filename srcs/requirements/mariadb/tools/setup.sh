29ca864b2414329378c6b44909010d0189da8cf7
Aamii16 <amina.zahir1610@gmail.com>
2026-09-08 03:55:45 +0100

    fixed access denied on mariadb

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
 
-service mariadb start
+mkdir -p /var/run/mysqld /var/lib/mysql
+chown 755 /var/run/mysqld
+chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 
-#uncomment this when you finish whole rpoject setup and compose
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "System tables missing. Initializing MariaDB system databases..."
+    mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+fi
 
-DB_PASSWORD="$(cat /run/secrets/db_password)"
-DB_ROOT_PASSWORD="$(cat /run/secrets/db_root_password)"
+if [ ! -f "/var/lib/mysql/.init" ]; then
+
+    service mariadb start
+    sleep 1 
+
+    DB_PASSWORD="$(cat /run/secrets/db_password)"
+    DB_ROOT_PASSWORD="$(cat /run/secrets/db_root_password)"
 
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;"
+    mysql -e "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\`;"
 
-mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
+    mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';"
 
-mysql -e "GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%';"
+    mysql -e "GRANT ALL PRIVILEGES ON \`$MYSQL_DATABASE\`.* TO '$MYSQL_USER'@'%';"
 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';"
+    mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASSWORD';"
 
-mysql -e "FLUSH PRIVILEGES;"
+    mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p$DB_ROOT_PASSWORD shutdown
+    touch /var/lib/mysql/.init
 
+    service mariadb stop
+    sleep 1
+fi
 exec mysqld
